75c41d4a3afdc6fb406753a150cf5bb096ee6819
Matheo Franco <73678410+mrHann69@users.noreply.github.com>
2023-06-22 22:22:19 -0500

    cleaning backend branch

--- src/index.js
+++
@@ -1,3 +0,0 @@
-for(let i=0; i<= 20; i++) console.log("holaaaa estoy vivo!!!!!");
-for(let i=0; i<= 20; i++) console.log("holaaaa otra vez!!!!!");
-for(let i=0; i<= 20; i++) console.log("otra vez!!!!!");
--- src/models/utils.js
+++
@@ -1,8 +0,0 @@
-const validator = require('validator');
-
-async function customValidator(value){
-// custom validator function that checks if email domain exists
-if (!validator.isFQDN(value.split('@')[1])) {
-  throw new Error('Invalid email domain');
-}
-}
